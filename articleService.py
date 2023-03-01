189e5812ce92d4ffe29438957e2efa9b6d6a7145
qyf <1364692268@qq.com>
2023-03-01 17:34:44 +0800

    Fix bug where article was not saved if queryOneByArticleUrl returned None

--- articleService.py
+++ articleService.py
@@ -4,6 +4,7 @@ from dao.articleDAO import ArticleDAO
 
 def saveArticle(article: Article):
     oldArticle = ArticleDAO.queryOneByArticleUrl(article.source_url)
+    #
     if oldArticle is None:
         ArticleDAO.insert(article)
 

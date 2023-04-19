f672b0a62763c09c5ea4ed940c840183ddf2b4ac
qyf <1364692268@qq.com>
2023-04-19 18:05:34 +0800

    lock test

--- test/lock_test.py
+++ test/lock_test.py
@@ -1,4 +1,3 @@
-
 import threading
 import time
 
@@ -6,31 +5,41 @@ num = 0
 
 
 def add():
-    print("add...")
-    global num
-    for i in range(10_000_000):
+    for i in range(50):
+        lock.acquire()
+        global num
         a = num
+        print("a1 = ", a)
+        time.sleep(0.1)
         a = a + 1
         num = a
-    print("add end")
+        print("num1 = ", num)
+        lock.release()
+
 
 def sub():
-    print("sub...")
-    global num
-    for i in range(10_000_000):
-        num = num - 1
-        print("num = ", num)
+    for i in range(50):
+        lock.acquire()
+        global num
+        # print("num222 = ", num)
+        a = num
+        print("a2 = ", a)
+        time.sleep(0.1)
+        a = a - 1
+        num = a
+        print("num2 = ", num)
+        lock.release()
 
 
 if __name__ == "__main__":
+    lock = threading.Lock()
     subThread01 = threading.Thread(target=add)
     subThread02 = threading.Thread(target=sub)
 
     subThread01.start()
-    # subThread02.start()
+    subThread02.start()
 
     subThread01.join()
-    # subThread02.join()
-
+    subThread02.join()
 
     print("num result : %s" % num)

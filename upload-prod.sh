45493f7694aabe632a2552db6bc252a3114d67a1
qyfshizi <1364692268@qq.com>
2024-01-26 16:42:59 +0800

    rm info

--- upload-prod.sh
+++ upload-prod.sh
@@ -1,12 +1,12 @@
 
 expect -c "
-spawn scp ../article_spider.zip root@8.210.126.239:/home/freedomproject/article_spider.zip
+spawn scp ../article_spider.zip root@***:/home/freedomproject/article_spider.zip
 
 expect {
     \"*assword\"
                 {
                     set timeout 300;
-                    send \"Xianyadan123.\r\";
+                    send \"***\r\";
                 }
     \"yes/no\"
                 {
